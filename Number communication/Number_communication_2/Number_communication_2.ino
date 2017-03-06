c99e2427f5af5e11843edd4eeaad7872268784d7
adamlgarrigos <adamlaaouajg@gmail.com>
2017-03-06 19:21:36 +0100

    m

--- Number communication/Number_communication_2/Number_communication_2.ino
+++ Number communication/Number_communication_2/Number_communication_2.ino
@@ -9,7 +9,7 @@
 //***************************** INCLUDE **********************************
 
 //***************************** VARIABLES ********************************
-int n;             // variables for the R's
+long n;             // variables for the R's
 int r;// variables for the results
 
 //***************************** SETUP ************************************
@@ -26,23 +26,150 @@ void loop() {
     n = Serial.parseInt();// look for valid int the incoming serial stream
 
     if (Serial.read() == '\n') { //look for newline. Is the end of your sentence
-    r = n % 2;
+    r = n % 23;
 
-    Serial.print("El numero ");
+    Serial.print("El DNI es ");
     Serial.print(n);
     if (r == 0)
     {
-    Serial.println(" es parell");
+    Serial.println(" T ");
     Serial.println("");
     }
-    else 
+    else if (r == 1)
     {
-    Serial.println(" es senar");
+    Serial.println(" R ");
     Serial.println("");
     }
-    Serial.println("Entra un numero?");
+    else if (r == 2)
+    {
+    Serial.println(" W ");
     Serial.println("");
+    
     }
+    else if (r == 2)
+    {
+    Serial.println(" W ");
+    Serial.println("");
+    
+    }
+    else if (r == 3)
+    {
+    Serial.println(" A ");
+    Serial.println("");
+    
+    }
+    else if (r == 4)
+    {
+    Serial.println(" G ");
+    Serial.println("");
+    
+    }
+    else if (r == 5)
+    {
+    Serial.println(" M ");
+    Serial.println("");
+    
+    }
+    else if (r == 6)
+    {
+    Serial.println(" Y ");
+    Serial.println("");
+    
+    }
+    else if (r == 7)
+    {
+    Serial.println(" F ");
+    Serial.println("");
+    
+    }
+    else if (r == 8)
+    {
+    Serial.println(" P ");
+    Serial.println("");
+    
+    }
+    else if (r == 9)
+    {
+    Serial.println(" D ");
+    Serial.println("");
+    
+    }
+    else if (r == 10)
+    {
+    Serial.println(" X ");
+    Serial.println("");
+    
+    }
+    else if (r == 11)
+    {
+    Serial.println(" B ");
+    Serial.println("");
+    
+    }
+    else if (r == 12)
+    {
+    Serial.println(" N ");
+    Serial.println("");
+    
+    }
+    else if (r == 13)
+    {
+    Serial.println(" J ");
+    Serial.println("");
+    
+    }
+    else if (r == 14)
+    {
+    Serial.println(" Z ");
+    Serial.println("");
+    
+    }
+    else if (r == 15)
+    {
+    Serial.println(" S ");
+    Serial.println("");
+    
+    }else if (r == 16)
+    {
+    Serial.println(" Q ");
+    Serial.println("");
+    
+    }else if (r == 17)
+    {
+    Serial.println(" V ");
+    Serial.println("");
+    
+    }
+    else if (r == 18)
+    {
+    Serial.println(" H ");
+    Serial.println("");
+    
+    }
+    else if (r == 19)
+    {
+    Serial.println(" L ");
+    Serial.println("");
+    
+    }
+    else if (r == 20)
+    {
+    Serial.println(" C ");
+    Serial.println("");
+    
+    }else if (r == 21)
+    {
+    Serial.println(" K ");
+    Serial.println("");
+    
+    }else if (r == 22)
+    {
+    Serial.println(" E ");
+    Serial.println("");
+    
+    }
+    
   }
+}
 }
 //***************************** FUNCIONS *********************************
